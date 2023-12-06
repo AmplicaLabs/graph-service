2b2d815548746229a25ef3d24d87bb095288d145
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2023-12-06 09:53:45 +0530

    define surface level fetch api

--- apps/api/src/api.controller.ts
+++ apps/api/src/api.controller.ts
@@ -1,7 +1,10 @@
-import { Controller, Get, HttpStatus, Logger } from '@nestjs/common';
+import { Controller, Get, HttpCode, HttpStatus, Logger, Query } from '@nestjs/common';
+import { ApiBody, ApiOkResponse, ApiQuery, ApiTags } from '@nestjs/swagger';
 import { ApiService } from './api.service';
+import { GraphsQueryParamsDto } from '../../../libs/common/src';
 
 @Controller('api')
+@ApiTags('API') // Specify the tag for Swagger documentation
 export class ApiController {
   private readonly logger: Logger;
 
@@ -9,11 +12,30 @@ export class ApiController {
     this.logger = new Logger(this.constructor.name);
   }
 
+  // Health endpoint
   // eslint-disable-next-line class-methods-use-this
   @Get('health')
   health() {
     return {
       status: HttpStatus.OK,
     };
+  }
+
+  // Fetch graphs for list of `dsnpIds` at optional `blockNumber`
+  @Get('graphs')
+  @HttpCode(HttpStatus.OK)
+  @ApiOkResponse({ description: 'Graphs retrieved successfully' })
+  async getGraphs(@Query() queryParams: GraphsQueryParamsDto) {
+    try {
+      // TODO: Uncomment this line once the ApiService is implemented
+      // const graphs = await this.apiService.getGraphs(queryParams.dsnpIds, queryParams.blockNumber);
+      return {
+        status: HttpStatus.OK,
+        data: [],
+      };
+    } catch (error) {
+      this.logger.error(error);
+      throw new Error('Failed to fetch graphs');
+    }
   }
 }
--- libs/common/src/index.ts
+++ libs/common/src/index.ts
@@ -2,3 +2,4 @@ export * from './utils/processing';
 export * from './utils/redis';
 export * from './utils/base-consumer';
 export * from './constants';
+export * from './dtos/graph.query.dto';
