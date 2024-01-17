b9fe1589e70b68dfe53f32afd5e965455170e4cc
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2024-01-17 10:35:42 -0600

    setup for e2e tests

---
+++ apps/api/test/app.e2e-spec.ts
@@ -0,0 +1,31 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable no-undef */
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import request from 'supertest';
+import { EventEmitter2 } from '@nestjs/event-emitter';
+import { randomFill } from 'crypto';
+import { ApiModule } from '../src/api.module';
+
+describe('Graph Service E2E request verification!', () => {
+  let app: INestApplication;
+  let module: TestingModule;
+  // eslint-disable-next-line no-promise-executor-return
+  const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
+  beforeEach(async () => {
+    module = await Test.createTestingModule({
+      imports: [ApiModule],
+    }).compile();
+
+    app = module.createNestApplication();
+    const eventEmitter = app.get<EventEmitter2>(EventEmitter2);
+    eventEmitter.on('shutdown', async () => {
+      await app.close();
+    });
+    app.useGlobalPipes(new ValidationPipe());
+    app.enableShutdownHooks();
+    await app.init();
+  });
+
+  it('(GET) /api/health', () => request(app.getHttpServer()).get('/api/health').expect(200).expect({ status: 200 }));
+});
