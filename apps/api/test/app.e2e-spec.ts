888de7c6244009c1581a1c8c655f21066ee61e53
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2024-01-19 10:37:39 -0600

    add private graph test

--- apps/api/test/app.e2e-spec.ts
+++ apps/api/test/app.e2e-spec.ts
@@ -4,9 +4,8 @@ import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import request from 'supertest';
 import { EventEmitter2 } from '@nestjs/event-emitter';
-import { randomFill } from 'crypto';
 import { ApiModule } from '../src/api.module';
-import { PrivacyType, ProviderGraphDto } from '../../../libs/common/src';
+import { ConnectionDto, GraphKeyPairDto, KeyType, PrivacyType, ProviderGraphDto } from '../../../libs/common/src';
 import { Direction } from '../../../libs/common/src/dtos/direction.dto';
 import { ConnectionType } from '../../../libs/common/src/dtos/connection.type.dto';
 
@@ -33,19 +32,48 @@ describe('Graph Service E2E request verification!', () => {
   it('(GET) /api/health', () => request(app.getHttpServer()).get('/api/health').expect(200).expect({ status: 200, message: 'Service is healthy' }));
 
   describe('(POST) /api/update-graph', () => {
-    it('Valid graph update request should work', async () => {
+    it('Valid public graph update request should work', async () => {
       const validGraphChangeRequest: ProviderGraphDto = {
         dsnpId: '2',
         connections: {
           data: [
             {
-              dsnpId: '3',
+              dsnpId: '4',
               privacyType: PrivacyType.Public,
               direction: Direction.ConnectionTo,
               connectionType: ConnectionType.Follow,
-            },
+            } as ConnectionDto,
           ],
         },
+      };
+
+      return request(app.getHttpServer())
+        .post(`/api/update-graph`)
+        .send(validGraphChangeRequest)
+        .expect(201)
+        .expect((res) => expect(res.text).toContain('referenceId'));
+    });
+
+    it('Valid private graph update request should work', async () => {
+      const validGraphChangeRequest: ProviderGraphDto = {
+        dsnpId: '2',
+        connections: {
+          data: [
+            {
+              dsnpId: '4',
+              privacyType: PrivacyType.Private,
+              direction: Direction.ConnectionTo,
+              connectionType: ConnectionType.Follow,
+            } as ConnectionDto,
+          ],
+        },
+        graphKeyPairs: [
+          {
+            keyType: KeyType.X25519,
+            publicKey: '0x993052b57e8695d9124964f69f624fcc2080be7525c65b1acd089dff235a0e02',
+            privateKey: '0xf74d39829ac4a814048cbda6b35ee1c3c16fbd2b88f97d552aa344bffb5207a5',
+          } as GraphKeyPairDto
+        ]
       };
 
       return request(app.getHttpServer())
